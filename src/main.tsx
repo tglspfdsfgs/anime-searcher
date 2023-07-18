f8365915fa6c7dbbb35f1f66068b969d5558e6f9
tglspfdsfgs <tglspfdsfgs@gmail.com>
2023-07-18 13:39:30 +0300

    feat: replace Main block with router Outlet

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import '@globStyles/normalize.scss';
 import '@globStyles/global.scss';
 import Root from '@routes/Root';
 import ErrorPage from './routes/ErrorPage';
+import NotExistingRoute from '@components/NotExistingRoute';
+import Main from '@components/Main';
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -12,7 +14,12 @@ import {
 } from 'react-router-dom';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<Root />} errorElement={<ErrorPage />}></Route>)
+  createRoutesFromElements(
+    <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
+      <Route path="*" element={<NotExistingRoute />} />
+      <Route index element={<Main />} />
+    </Route>
+  )
 );
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
--- src/routes/Root/index.tsx
+++ src/routes/Root/index.tsx
@@ -1,12 +1,12 @@
 import Header from '@components/Header';
-import Main from '@components/Main';
+import { Outlet } from 'react-router-dom';
 import Footer from '@components/Footer';
 
 export default function Root() {
   return (
     <>
       <Header />
-      <Main />
+      <Outlet />
       <Footer />
     </>
   );
