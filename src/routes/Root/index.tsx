b2cc5cd34b5e8208a4c6ae581ccb553f8b05bbca
tglspfdsfgs <tglspfdsfgs@gmail.com>
2023-07-20 21:24:17 +0300

    feat: add useLoaderData, pass its state to Outlet prop

--- src/routes/Root/index.tsx
+++ src/routes/Root/index.tsx
@@ -1,12 +1,14 @@
 import Header from '@components/Header';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLoaderData } from 'react-router-dom';
 import Footer from '@components/Footer';
+import { ResObect } from '@interfaces/response';
 
 export default function Root() {
+  const res = useLoaderData() as ResObect;
   return (
     <>
       <Header />
-      <Outlet />
+      <Outlet context={res} />
       <Footer />
     </>
   );
