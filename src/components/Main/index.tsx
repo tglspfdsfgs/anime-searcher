3344b3482b743004a8475233c2d0cf0673f6f401
tglspfdsfgs <tglspfdsfgs@gmail.com>
2023-07-20 21:33:47 +0300

    feat: extract response data, pass it as AnimeCard props

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,22 @@
+import { useOutletContext } from 'react-router-dom';
 import styles from './styles.module.scss';
 import AnimeCard from '@components/AnimeCard';
+import { ResObect } from '@interfaces/response';
 
 export default function Main() {
-  return <main className={styles.contentHolder}>{new Array(12).fill(<AnimeCard />)}</main>;
+  const res = useOutletContext() as ResObect | null;
+  let cardList;
+  if (res?.data) {
+    res.data.pop();
+    cardList = res.data.map((item, i) => (
+      <AnimeCard
+        key={i}
+        image={item.images.jpg?.image_url}
+        score={item.score}
+        title={item.title}
+        overview={item.synopsis}
+      />
+    ));
+  }
+  return <main className={styles.contentHolder}>{cardList}</main>;
 }
