3988ff173a07112cd61562bd10815b4e7694f54f
tglspfdsfgs <tglspfdsfgs@gmail.com>
2023-07-20 21:36:21 +0300

    feat: replace AnimeCard placeholders with props, add poster styles

--- src/components/AnimeCard/index.tsx
+++ src/components/AnimeCard/index.tsx
@@ -1,23 +1,27 @@
 import styles from './styles.module.scss';
 
-export default function AnimeCard() {
+interface Props {
+  image?: string | null;
+  score: number | null;
+  title: string;
+  overview: string | null;
+}
+
+export default function AnimeCard({ image, score, title, overview }: Props) {
   return (
     <article className={styles.movieCard}>
-      <img src="notavaliable.jpg" alt="Poster not avaliable" />
+      <img
+        className={styles.poster}
+        src={image || 'notavaliable.jpg'}
+        alt={title || 'Poster not avaliable'}
+      />
       <div className={styles.info}>
-        <h3 className={styles.title}>Title</h3>
-        <div className={styles.score}>0.0</div>
+        <h3 className={styles.title}>{title}</h3>
+        <div className={styles.score}>{score !== null ? score : 'N/A'}</div>
       </div>
       <div className={styles.overview}>
         <h3 className={styles.overviewHeader}>Overview</h3>
-        <p className={styles.overviewText}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum.
-        </p>
+        <p className={styles.overviewText}>{overview}</p>
       </div>
     </article>
   );
