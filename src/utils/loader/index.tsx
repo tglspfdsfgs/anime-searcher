c9f66d44b7cebb77687c88d702e4d0abdf8fbfb6
tglspfdsfgs <tglspfdsfgs@gmail.com>
2023-07-21 19:22:37 +0300

    fix: eliminate possibility of empty search request

--- src/utils/loader/index.tsx
+++ src/utils/loader/index.tsx
@@ -2,7 +2,7 @@ import fetchData from '@utils/fetchData';
 
 export default async function loader({ request }: { request: Request }) {
   const url = new URL(request.url);
-  const query = url.searchParams.get('q');
+  const query = url.searchParams.get('q')?.trim();
   let res;
   if (query) {
     res = fetchData(`https://api.jikan.moe/v4/anime?q=${query}`);
