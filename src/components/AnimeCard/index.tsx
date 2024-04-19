132718abaff7799424eac4e7b3496330ff9982ed
tglspfdsfgs <tglspfdsfgs@gmail.com>
2024-04-19 11:17:57 +0300

    feat: add score highlighting

--- src/components/AnimeCard/index.tsx
+++ src/components/AnimeCard/index.tsx
@@ -8,6 +8,18 @@ interface Props {
 }
 
 export default function AnimeCard({ image, score, title, overview }: Props) {
+  let scoreHighlight = '';
+
+  if (score !== null) {
+    if (score <= 3) {
+      scoreHighlight = styles.lowScore;
+    } else if (score <= 7) {
+      scoreHighlight = styles.mediumScore;
+    } else {
+      scoreHighlight = styles.highScore;
+    }
+  }
+
   return (
     <article className={styles.movieCard}>
       <img
@@ -17,7 +29,7 @@ export default function AnimeCard({ image, score, title, overview }: Props) {
       />
       <div className={styles.info}>
         <h3 className={styles.title}>{title}</h3>
-        <div className={styles.score}>{score !== null ? score : 'N/A'}</div>
+        <div className={styles.score + ' ' + scoreHighlight}>{score !== null ? score : 'N/A'}</div>
       </div>
       <div className={styles.overview}>
         <h3 className={styles.overviewHeader}>Overview</h3>
