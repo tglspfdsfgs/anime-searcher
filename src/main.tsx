17fb801970b11d593c6420888670af4481265e18
tglspfdsfgs <tglspfdsfgs@gmail.com>
2024-04-23 10:59:19 +0300

    fix: remove Not Existing Route component

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import '@globStyles/normalize.scss';
 import '@globStyles/global.scss';
 import Root from '@routes/Root';
 import ErrorPage from './routes/ErrorPage';
-import NotExistingRoute from '@components/NotExistingRoute';
 import Main from '@components/Main';
 import {
   createBrowserRouter,
@@ -27,7 +26,6 @@ window.basePath = document.location.pathname === '/' ? '' : document.location.pa
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path={window.basePath || '/'} element={<Root />} errorElement={<ErrorPage />}>
-      <Route path={window.basePath + '/*'} element={<NotExistingRoute />} />
       <Route index element={<Main />} loader={mainLoader} />
       <Route path={window.basePath + '/reviews'} element={<Reviews />} loader={reviewsLoader} />
     </Route>
